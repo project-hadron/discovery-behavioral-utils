5f2ac5a564bf6c75efc10098ab850980733bf33a
doatridge <darryl@opengrass.io>
2019-03-31 22:15:01 -0500

    - Correction of GenericSample

--- ds_behavioral/sample/sample_data.py
+++ ds_behavioral/sample/sample_data.py
@@ -149,7 +149,7 @@ class ProfileSample(AbstractSample):
         :param seed: (optional) a seed value
         :return: a list of names
         """
-        selection = pd.Series[
+        selection = [
              "aol.com", "att.net", "comcast.net", "facebook.com", "gmail.com", "gmx.com", "googlemail.com",
              "google.com", "hotmail.com", "mac.com", "me.com", "mail.com", "msn.com", "live.com", "sbcglobal.net",
              "verizon.net", "yahoo.com", "email.com", "fastmail.fm", "games.com", "gmx.net", "hush.com", "hushmail.com",
